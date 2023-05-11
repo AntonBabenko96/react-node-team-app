3c799a9b6ada6c55d5dd1025c937689e35790c7a
Ihor Samburenko <115349844+Ihor-Samburenko@users.noreply.github.com>
2023-05-11 20:27:47 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-// import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
-import NoticesSearch from 'components/NoticesSearch/NoticesSearch';
+
 
 
 export const App = () => {
-  return <>
-  <SharedLayout />
-  <NoticesSearch/>
-  </>
-
+  return (
+    <>
+      <BrowserRouter basename="/react-node-team-app">
+        <SharedLayout />
+      </BrowserRouter>
+    </>
+  );
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,4 +1,7 @@
 import Header from 'components/Header/Header';
+import NoticesSearch from 'components/NoticesSearch/NoticesSearch';
+import { Route, Routes } from 'react-router-dom';
+import UserPage from 'pages/UserPage/UserPage';
 
 
 
@@ -6,6 +9,10 @@ export default function SharedLayout() {
   return (
     <>
       <Header />
+      <NoticesSearch/>
+      <Routes>
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
     </>
   );
 }
