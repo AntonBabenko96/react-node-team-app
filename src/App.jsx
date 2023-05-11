112cfa1e1b90afae3bd1de1bb064590578274d06
Ihor Samburenko <115349844+Ihor-Samburenko@users.noreply.github.com>
2023-05-11 20:13:21 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@
 // import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 import SharedLayout from './components/SharedLayout/SharedLayout';
+import NoticesSearch from 'components/NoticesSearch/NoticesSearch';
+
 
 export const App = () => {
-  return <SharedLayout />;
+  return <>
+  <SharedLayout />
+  <NoticesSearch/>
+  </>
+
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,7 @@
 import Header from 'components/Header/Header';
 
+
+
 export default function SharedLayout() {
   return (
     <>
