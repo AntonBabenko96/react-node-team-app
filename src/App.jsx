8e643b7fc9c02bd4ca9dc6377fb05214c09b5f57
AntonBabenko96 <ababenko159@gmail.com>
2023-05-11 11:16:07 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
-import SharedLayout from 'pages/SharedLayout/SharedLayout';
+import SharedLayout from './components/SharedLayout/SharedLayout';
 
 export const App = () => {
   return <SharedLayout />;
