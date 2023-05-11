c5dfef68cbc22d3333a2aa0c3f0793a07b2afda5
AntonBabenko96 <ababenko159@gmail.com>
2023-05-11 21:04:33 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-
 import { BrowserRouter } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 
-
-
 export const App = () => {
-  return  <BrowserRouter>
-  <SharedLayout  />
-</BrowserRouter>
   return (
-    <>
-      <BrowserRouter basename="/react-node-team-app">
-        <SharedLayout />
-      </BrowserRouter>
-    </>
+    <BrowserRouter basename="/react-node-team-app">
+      <SharedLayout />
+    </BrowserRouter>
   );
 };
