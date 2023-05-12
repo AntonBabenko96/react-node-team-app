a52da4699ddcb706b7cfd45b4e4f275ad34fbe40
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-05-12 20:48:01 +0300

    done

--- src/api/news.js
+++ src/api/news.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 const baseURL = process.env.REACT_APP_BASE_URL;
 
 const newsInstance = axios.create({
-  baseURL,
+  baseURL: baseURL ? baseURL : 'https://your-pet-backend.onrender.com/',
 });
 
-export const getNews = async () => {
+export const getNews = async (page = 1, limit = 10) => {
   try {
-    const { data } = await newsInstance.get('/news');
+    const { data } = await newsInstance.get(
+      `/news?page=${page}&limit=${limit}`
+    );
     return data;
   } catch (error) {
     throw error;
