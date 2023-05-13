a8c86e2900c4d09bda6158c58fa53f993e44597c
Your Name <wtm@hotmail.com.au>
2023-05-13 12:07:39 +0300

    Login/register Pages

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,3 +1,18 @@
+import { AuthForm } from 'components/AuthForm/AuthForm';
+
+import styles from '../MainPage/MainPage.module.scss';
+import style from './RegisterPage.module.scss'
+
 export default function RegisterPage() {
-  return <></>;
+  return (
+    <>
+      <section className={styles.MainSection}>
+        <div className="container">
+          <div className={style.form}>
+            <AuthForm />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
