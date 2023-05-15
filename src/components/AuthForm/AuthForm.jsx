1efba52b60b5b0f21c66c5ba67dca6155bfd7df3
Your Name <wtm@hotmail.com.au>
2023-05-15 14:45:20 +0300

    ConratsModal (initial version) / deleted commented code from AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,35 +1,28 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-// import PetsIcon from '@mui/icons-material/Pets';
 import CloseIcon from '@mui/icons-material/Close';
 
-// import Modal from '../Modal/Modal';
 import {
   validationRegisterSchema,
   validationLoginSchema,
 } from 'helpers/validationSchema';
-
-
 import { register, login } from '../../redux/auth/auth-operations';
-// import { selectIsLogin, selectLoading } from '../../redux/auth/selectors';
 
 import './AuthForm.scss';
 
 export const AuthForm = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  // const [showModal, setShowModal] = useState(false);
+
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  // const isLogin = useSelector(selectIsLogin);
-  // const isLoading = useSelector(selectLoading);
 
   const [currentPath] = useState(window.location.pathname);
   const isRegisterPath = currentPath.endsWith('/register');
@@ -203,8 +196,6 @@ export const AuthForm = () => {
         )}
       </Formik>
 
-     
-
       {isRegisterPath ? (
         <p>
           Already have an account? <Link to="/login"> Login</Link>{' '}
@@ -214,16 +205,6 @@ export const AuthForm = () => {
           Don't have an account? <Link to="/register"> Register</Link>{' '}
         </p>
       )}
-
-      {/* {showModal && !isLoading && isLogin && (
-        <Modal onClose={() => setShowModal(false)} style={{ width: '608px' }}>
-          <h2 className="modalH">Congrats!</h2>
-          <p className="modalP">Your registration is successful</p>
-          <button className="modalBtn" onClick={() => navigate('/user')}>
-            Go to profile <PetsIcon className="modalIcon" />
-          </button>
-        </Modal>
-      )} */}
     </div>
   );
 };
