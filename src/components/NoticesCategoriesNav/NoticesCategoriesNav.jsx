197d086a6185091d216f6d9aa669ebf8800a0d2e
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-05-16 14:51:11 +0300

    navCateg

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,12 +1,38 @@
-const mainCategoriesList = [{id: 1, name: "sell"}, {id: 2, name: "lost/found"}, {id: 3, name: "in good hands"}];
-const userCategoriesList = [{id: 4, name: "favorite ads"}, {id: 5, name: "my adss"}];
+import Style from "./NoticesCategoriesNav.module.scss"
+import {getNotices,getMyNotices} from "../../redux/notices/notices-operations"
+import { useDispatch,useSelector } from 'react-redux';
+import { selectIsLogin} from "../../redux/auth/selectors";
+const mainCategoriesList = [{ id: 1, value: "sell", name: "sell" }, { id: 2,value: "lost-found", name: "lost/found" }, { id: 3,value: "for-free", name: "in good hands" }];
+const userCategoriesList = [{id: 4,value: "favorite-ads", name: "favorite ads"}, {id: 5, value: "my-adss", name: "my adss"}];
 
-export default function NoticesCategoriesNav() {
-    const mainCategories = mainCategoriesList.map(({id, name}) => <button key={id} type="button">{name}</button>)
-    const userCategories = userCategoriesList.map(({id, name}) => <button key={id} type="button">{name}</button>)
 
-    return<div>
-    {mainCategories}
-    {userCategories}
+export default function NoticesCategoriesNav(value) {
+    const dispatch = useDispatch();
+    const isLogin = useSelector(selectIsLogin);
+    const filterСhange = (e) => {
+        let value = e.target.name;
+        const fetch = {
+            category: value,
+            page: 1,
+            limit: 20,
+        }
+        dispatch(getNotices(fetch)); 
+    }
+    const filterСhangeUser = (e) => {
+        let value = e.target.name;
+        // const fetch = {
+        //     category: value,
+        //     page: 1,
+        //     limit: 20,
+        // }
+        // dispatch(getMyNotices(fetch)); 
+    }
+
+    const mainCategories = mainCategoriesList.map(({id, value, name}) => <button key={id} name={value} type="button" className={Style.categoriesbtn} onClick={filterСhange}>{name}</button>)
+    const userCategories = userCategoriesList.map(({id,value, name}) => <button key={id} name={value} type="button" className={Style.categoriesbtn} onClick={filterСhangeUser}>{name}</button>)
+
+    return<div className={Style.filterCategiries}>
+        {mainCategories}
+        {isLogin && userCategories}
     </div>
 }
