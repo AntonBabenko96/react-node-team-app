d13973bdbce5cef504a33fdd16bc37d78c5f2fb1
Anton Orlov <anton_orlov@ukr.net>
2023-05-16 20:50:44 +0300

    page notfound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,20 @@
-export default function NotFound() {
-  return <div>Not Found</div>;
-}
+import { Link } from 'react-router-dom';
+import css from './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <section className={css.bgnotfound}>
+      <div className="container">
+        <p className={css.text}>
+          Ooops! <br></br>This page not found :(
+        </p>
+        <div className={css.wrapper}></div>
+        <Link to="/" className={css.link}>
+          To main page
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
