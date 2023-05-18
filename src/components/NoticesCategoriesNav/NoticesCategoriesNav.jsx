a1b3d7679db4873799fa69854ff5f15169a0b2f8
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-05-18 14:47:24 +0300

    nav

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,38 +1,76 @@
-import Style from "./NoticesCategoriesNav.module.scss"
-import {getNotices} from "../../redux/notices/notices-operations"
-import { useDispatch,useSelector } from 'react-redux';
-import { selectIsLogin} from "../../redux/auth/selectors";
-const mainCategoriesList = [{ id: 1, value: "sell", name: "sell" }, { id: 2,value: "lost-found", name: "lost/found" }, { id: 3,value: "for-free", name: "in good hands" }];
-const userCategoriesList = [{id: 4,value: "favorite-ads", name: "favorite ads"}, {id: 5, value: "my-adss", name: "my adss"}];
+import Style from './NoticesCategoriesNav.module.scss';
+import {
+  getNotices,
+  getFavoritesList,
+  getMyNotices,
+} from '../../redux/notices/notices-operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLogin } from '../../redux/auth/selectors';
+import { NavLink } from 'react-router-dom';
 
+const mainCategoriesList = [
+  { id: 1, value: 'sell', name: 'sell' },
+  { id: 2, value: 'lost-found', name: 'lost/found' },
+  { id: 3, value: 'for-free', name: 'in good hands' },
+];
+const userCategoriesList = [
+  { id: 4, value: 'favorite-ads', name: 'favorite ads' },
+  { id: 5, value: 'my-adss', name: 'my adss' },
+];
 
-export default function NoticesCategoriesNav(value) {
-    const dispatch = useDispatch();
-    const isLogin = useSelector(selectIsLogin);
-    const filterСhange = (e) => {
-        let value = e.target.name;
-        const fetch = {
-            category: value,
-            page: 1,
-            limit: 20,
-        }
-        dispatch(getNotices(fetch)); 
+export default function NoticesCategoriesNav() {
+  const dispatch = useDispatch();
+  const isLogin = useSelector(selectIsLogin);
+  const filterСhange = e => {
+    let value = e.target.name;
+    const fetch = {
+      category: value,
+      page: 1,
+      limit: 20,
+    };
+    dispatch(getNotices(fetch));
+  };
+  const filterСhangeUser = e => {
+    switch (e.target.name) {
+      case 'favorite-ads':
+        dispatch(getFavoritesList());
+        break;
+      default:
+        dispatch(getMyNotices());
+        break;
     }
-    const filterСhangeUser = (e) => {
-        // let value = e.target.name;
-        // const fetch = {
-        //     category: value,
-        //     page: 1,
-        //     limit: 20,
-        // }
-        // dispatch(getMyNotices(fetch)); 
-    }
+  };
 
-    const mainCategories = mainCategoriesList.map(({id, value, name}) => <button key={id} name={value} type="button" className={Style.categoriesbtn} onClick={filterСhange}>{name}</button>)
-    const userCategories = userCategoriesList.map(({id,value, name}) => <button key={id} name={value} type="button" className={Style.categoriesbtn} onClick={filterСhangeUser}>{name}</button>)
+  const mainCategories = mainCategoriesList.map(({ id, value, name }) => (
+    <NavLink to={`/notices/${value}`} key={id}>
+      <button
+        name={value}
+        type="button"
+        className={Style.categoriesbtn}
+        onClick={filterСhange}
+      >
+        {name}
+      </button>
+    </NavLink>
+  ));
+  const userCategories = userCategoriesList.map(({ id, value, name }) => (
+    <NavLink to={`/notices/${value}`} key={id}>
+      <button
+      key={id}
+      name={value}
+      type="button"
+      className={Style.categoriesbtn}
+      onClick={filterСhangeUser}
+    >
+      {name}
+    </button>
+    </NavLink>
+  ));
 
-    return<div className={Style.filterCategiries}>
-        {mainCategories}
-        {isLogin && userCategories}
+  return (
+    <div className={Style.filterCategiries}>
+      {mainCategories}
+      {isLogin && userCategories}
     </div>
+  );
 }
