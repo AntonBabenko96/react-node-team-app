f23e73eb89743efaa7ff2dce2334fec75d0846f8
Zhvanko Anna <zhvankoann@gmail.com>
2023-05-22 23:49:11 +0300

    Another try

--- src/components/Modal/NoticeModal/NoticeModal.jsx
+++ src/components/Modal/NoticeModal/NoticeModal.jsx
@@ -1,17 +1,11 @@
 import { getDateFormat } from 'shared/utils/getDateFormat';
-import {ReactComponent as Heart} from "img/svg/heart.svg";
+import { ReactComponent as Heart } from 'img/svg/heart.svg';
 import placeholder from 'img/placeholder.png';
 
 import Style from './NoticeModal.module.scss';
 
-
 let email = '';
 let phone = '';
-const categoryItems = [
-  { name: 'sell', value: 'sell' },
-  { name: 'lost-found', value: 'lost/found' },
-  { name: 'for-free', value: 'in good hands' },
-];
 
 export default function NoticeModal({
   _id,
@@ -34,74 +28,77 @@ export default function NoticeModal({
     phone = owner.phone ? owner.phone : 'no data';
   }
 
-  if(category) {
-  const categoryItem = categoryItems.find(item => item.name === category);
-  category = categoryItem.value;
+  if (category === 'lost-found') {
+    category = 'lost/found';
+  }
+
+  if (category === 'for-free') {
+    category = 'in good hands';
   }
 
   return (
-      <div className={Style.wrapper}>
-        <div className={Style.Notice}>
-          <div className={Style.NoticePhotoWrapper}>
-            <img
-              className={Style.NoticePhoto}
-              src={photoURL ? `${photoURL}` : placeholder}
-              alt="Pet"
-            />
-            <p className={Style.StickyValue}>{category}</p>
-          </div>
-          <div className={Style.WrapperText}>
-            <h3 className={Style.NoticeTitle}>{title}</h3>
-            <ul className={Style.NoticeList}>
-              <li>
-                <h4>Name:</h4>
-                <p>{name}</p>
-              </li>
-              <li>
-                <h4>Type:</h4>
-                <p>{type}</p>
-              </li>
-              <li>
-                <h4>Birthday:</h4>
-                <p>{date}</p>
-              </li>
-              <li>
-                <h4>Breed:</h4>
-                <p>{breed}</p>
-              </li>
-              <li>
-                <h4>Place:</h4>
-                <p>{location}</p>
-              </li>
-              <li>
-                <h4>The sex:</h4>
-                <p>{sex}</p>
-              </li>
-              <li>
-                <h4>Email:</h4>
-                <a href={`mailto: ${email}`}>{email}</a>
-              </li>
-              <li>
-                <h4>Phone:</h4>
-                <a href={`tel: ${phone}`}>{phone}</a>
-              </li>
-            </ul>
-          </div>
+    <div className={Style.wrapper}>
+      <div className={Style.Notice}>
+        <div className={Style.NoticePhotoWrapper}>
+          <img
+            className={Style.NoticePhoto}
+            src={photoURL ? `${photoURL}` : placeholder}
+            alt="Pet"
+          />
+          <p className={Style.StickyValue}>{category}</p>
         </div>
-        <p className={Style.NoticeComent}>
-          <strong>Comments:</strong> {comments}
-        </p>
-        <div className={Style.ButtonWrapper}>
-          <button className={`${Style.ButtonContact} ${Style.button}`}>
-            <a href={`tel: ${phone}`}>Contact</a>
-          </button>
-          <button
-            className={`${Style.ButtonFavorite} ${Style.button}`}
-            onClick={() => onFavoriteBtnClick(_id)}
-          >
-            Add to <Heart className={Style.icon} />
-          </button>
+        <div className={Style.WrapperText}>
+          <h3 className={Style.NoticeTitle}>{title}</h3>
+          <ul className={Style.NoticeList}>
+            <li>
+              <h4>Name:</h4>
+              <p>{name}</p>
+            </li>
+            <li>
+              <h4>Type:</h4>
+              <p>{type}</p>
+            </li>
+            <li>
+              <h4>Birthday:</h4>
+              <p>{date}</p>
+            </li>
+            <li>
+              <h4>Breed:</h4>
+              <p>{breed}</p>
+            </li>
+            <li>
+              <h4>Place:</h4>
+              <p>{location}</p>
+            </li>
+            <li>
+              <h4>The sex:</h4>
+              <p>{sex}</p>
+            </li>
+            <li>
+              <h4>Email:</h4>
+              <a href={`mailto: ${email}`}>{email}</a>
+            </li>
+            <li>
+              <h4>Phone:</h4>
+              <a href={`tel: ${phone}`}>{phone}</a>
+            </li>
+          </ul>
         </div>
       </div>
+      <p className={Style.NoticeComent}>
+        <strong>Comments:</strong> {comments}
+      </p>
+      <div className={Style.ButtonWrapper}>
+        <button className={`${Style.ButtonContact} ${Style.button}`}>
+          <a href={`tel: ${phone}`}>Contact</a>
+        </button>
+        <button
+          className={`${Style.ButtonFavorite} ${Style.button}`}
+          onClick={() => onFavoriteBtnClick(_id)}
+        >
+          Add to <Heart className={Style.icon} />
+        </button>
+      </div>
+    </div>
   );
 }
